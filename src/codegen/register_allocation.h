12acb740460680a4a39e652d0371292e4dab16bd
NorthernL1ghts <noreply@local>
2025-11-15 16:32:55 +0000

    [RA] Refactoring

--- src/codegen/register_allocation.h
+++ src/codegen/register_allocation.h
@@ -4,30 +4,30 @@
 #include <codegen.h>
 
 typedef struct RegisterAllocationInfo {
-    CodegenContext* context;
+  CodegenContext *context;
 
-    size_t register_count;
-    Register* registers;
+  size_t register_count;
+  Register *registers;
 
-    size_t argument_register_count;
-    Register* argument_registers;
+  size_t argument_register_count;
+  Register *argument_registers;
 
-    // In which register every function result ends up.
-    Register result_register;
+  // In which register every function result ends up.
+  Register result_register;
 
-    int64_t (*instruction_register_interference)(IRInstruction* instruction);
+  size_t (*instruction_register_interference)(IRInstruction *instruction);
 } RegisterAllocationInfo;
 
-RegisterAllocationInfo* ra_allocate_info
-(CodegenContext* context,
-Register result_register,
-size_t general_registers_count,
-Register* general_registers,
-size_t argument_registers_count,
-Register* argument_registers,
-int64_t (*instruction_register_interference)(IRInstruction* instruction)
-);
+RegisterAllocationInfo *ra_allocate_info
+(CodegenContext *context,
+ Register result_register,
+ size_t general_registers_count,
+ Register *general_registers,
+ size_t argument_registers_count,
+ Register *argument_registers,
+ size_t instruction_register_interference(IRInstruction *instruction)
+ );
 
-void ra(RegisterAllocationInfo* info);
+void ra(RegisterAllocationInfo *info);
 
 #endif /* ALY_COMPILER_REGISTER_ALLOCATION_H */
--- src/error.c
+++ src/error.c
@@ -8,93 +8,93 @@
 Error ok = { ERROR_NONE, NULL };
 
 void print_error(Error err) {
-    if (err.type == ERROR_NONE) {
-        return;
-    }
-    printf("ERROR: ");
-    ASSERT(ERROR_MAX == 6);
-    switch (err.type) {
-    default:
-        printf("Unkown error type...");
-        break;
-    case ERROR_TODO:
-        printf("TODO (not implemented)");
-        break;
-    case ERROR_SYNTAX:
-        printf("Invalid syntax");
-        break;
-    case ERROR_TYPE:
-        printf("Mismatched types");
-        break;
-    case ERROR_ARGUMENTS:
-        printf("Invalid arguments");
-        break;
-    case ERROR_GENERIC:
-        break;
-    case ERROR_NONE:
-        break;
-    }
-    putchar('\n');
-    if (err.msg) {
-        printf("     : %s\n", err.msg);
-    }
+  if (err.type == ERROR_NONE) {
+    return;
+  }
+  printf("ERROR: ");
+  ASSERT(ERROR_MAX == 6);
+  switch (err.type) {
+  default:
+    printf("Unkown error type...");
+    break;
+  case ERROR_TODO:
+    printf("TODO (not implemented)");
+    break;
+  case ERROR_SYNTAX:
+    printf("Invalid syntax");
+    break;
+  case ERROR_TYPE:
+    printf("Mismatched types");
+    break;
+  case ERROR_ARGUMENTS:
+    printf("Invalid arguments");
+    break;
+  case ERROR_GENERIC:
+    break;
+  case ERROR_NONE:
+    break;
+  }
+  putchar('\n');
+  if (err.msg) {
+    printf("     : %s\n", err.msg);
+  }
 }
 
 NORETURN
-static void vpanic(int code, const char* fmt, va_list args) {
-    fprintf(stderr, "Panic: ");
-    vfprintf(stderr, fmt, args);
-    fputc('\n', stderr);
-    exit(code);
+static void vpanic(int code, const char *fmt, va_list args) {
+  fprintf(stderr, "Panic: ");
+  vfprintf(stderr, fmt, args);
+  fputc('\n', stderr);
+  _Exit(code);
 }
 
-void panic(const char* fmt, ...) {
-    va_list va;
-    va_start(va, fmt);
-    vpanic(1, fmt, va);
-    exit(1); // unreachable
+void panic(const char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vpanic(1, fmt, va);
+  UNREACHABLE();
 }
 
-void panic_with_code(int code, const char* fmt, ...) {
-    va_list va;
-    va_start(va, fmt);
-    vpanic(code, fmt, va);
-    exit(1); // unreachable
+void panic_with_code(int code, const char *fmt, ...) {
+  va_list va;
+  va_start(va, fmt);
+  vpanic(code, fmt, va);
+  UNREACHABLE();
 }
 
-void assert_impl(
-const char* file,
-const char* func,
-int line,
-const char* condition,
-const char* fmt,
-...
+void assert_impl (
+    const char *file,
+    const char *func,
+    int line,
+    const char *condition,
+    const char *fmt,
+    ...
 ) {
-    /// Prettier file name
+/// Prettier file name
 #ifndef _MSC_VER
-    const char path_separator = '/';
+  const char path_separator = '/';
 #else
-    const char path_separator = '\\';
+  const char path_separator = '\\';
 #endif
-    const char* basename = strrchr(file, path_separator);
-    file = basename ? basename + 1 : file;
+  const char *basename = strrchr(file, path_separator);
+  file = basename ? basename + 1 : file;
 
-    fprintf(stderr, "Assertion failed: %s\n", condition);
-    fprintf(stderr, "    In file %s:%d\n", file, line);
-    fprintf(stderr, "    In function %s", func);
+  fprintf(stderr, "Assertion failed: %s\n", condition);
+  fprintf(stderr, "    In file %s:%d\n", file, line);
+  fprintf(stderr, "    In function %s", func);
 
-    if (strcmp(fmt, "!") != 0) {
-        fprintf(stderr, "\n    Message: ");
+  if (strcmp(fmt, "!") != 0) {
+    fprintf(stderr, "\n    Message: ");
 
-        // Skip '!' placeholder for non-empty format string.
-        fmt += 1;
+    // Skip '!' placeholder for non-empty format string.
+    fmt += 1;
 
-        va_list va;
-        va_start(va, fmt);
-        vfprintf(stderr, fmt, va);
-        va_end(va);
-    }
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stderr, fmt, va);
+    va_end(va);
+  }
 
-    fputc('\n', stderr);
-    exit(1);
+  fputc('\n', stderr);
+  _Exit(1);
 }
